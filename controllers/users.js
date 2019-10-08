16af349483e190e8ae982600a6aee2735f5c8f2b
Egor Grinevich <egor.grinevich1999@gmail.com>
2019-10-08 22:52:15 +0300

    4

--- controllers/users.js
+++ controllers/users.js
@@ -1,6 +1,9 @@
 module.exports = {
     singUp: async (req, res, next) => {
-      console.log('UserController.singUp() called')
+       // Email & Password
+    // req.value.body
+    console.log('contents of req.value.body', req.value.body);
+    console.log('UsersController.signUp() called!');
     },
 
     singIn: async (req, res, next) => {
--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,12 @@
+
 const express = require('express');
 const router = require('express-promise-router')();
 
+const { validateBody, schemas  } = require('../helpers/routeHelpers');
 const UsersController = require('../controllers/users');
 
 router.route('/singup')
-    .post(UsersController.singUp);
+    .post(validateBody(schemas.authSchema), UsersController.singUp);//при переходе на роут вызываем validateBody, if все ок вызываем singUp
 
 router.route('/singin')
     .post(UsersController.singIn);
